47668b55e22f21f797969f95a98d865f4363f4d2
d-santomauro <91647391+d-santomauro@users.noreply.github.com>
2023-02-02 16:08:55 +1000

    Peer review: Remove treatments that are not recom.

--- step1_data_prep.R
+++ step1_data_prep.R
@@ -18,9 +18,7 @@ dataset <- unique(dataset, by = c("Study.Name", "yi_round", "vi_round"))
 
 ## Create intervention dummy variables ##
 dataset[, `:=` (d_cognitive_therapy = 0, d_behavioural_therapy = 0, d_antidepressants = 0, 
-                d_antipsychotics = 0, d_azapirones = 0, d_anticonvulsants = 0, 
-                d_benzodiazepines = 0, d_neurokinin = 0, d_betablocker = 0, d_lithium = 0, 
-                d_antihistamine = 0, d_cycloserine = 0, d_psychodynamic = 0, d_supportive = 0)]
+                d_psychodynamic = 0, d_supportive = 0)]
 
 # Psychotherapy
 dataset[grepl("CBT", Intervention), `:=` (d_cognitive_therapy = 1, d_behavioural_therapy = 1)]
@@ -32,26 +30,15 @@ dataset[grepl("Exposure therapy", Intervention), `:=` (d_behavioural_therapy = 1
 # Medications
 antidepressants <- c("Noradrenalin Reuptake Inhibitors", "Tetracyclic antidepressants", "SARI", "SNRI", "SSRI", "Monoamine oxidase inhibitors", "Tricyclic antidepressants")
 for(i in antidepressants){
-  dataset[grepl(i, Intervention), `:=` (d_antidepressants = 1)]  
+  dataset[grepl(i, Intervention), `:=` (d_antidepressants = d_antidepressants+1)]
+  dataset[grepl(i, Comparison), `:=` (d_antidepressants = d_antidepressants-1)]  
 }
-dataset[grepl("Antipsychotics", Intervention), `:=` (d_antipsychotics = 1)]
-dataset[grepl("Azapirones", Intervention), `:=` (d_azapirones = 1)]
-dataset[grepl("Anticonvulsants", Intervention), `:=` (d_anticonvulsants = 1)]
-dataset[grepl("Benzodiazepines", Intervention), `:=` (d_benzodiazepines = 1)]
-dataset[grepl("Neurokinin", Intervention), `:=` (d_neurokinin  = 1)]
-dataset[grepl("Beta", Intervention), `:=` (d_betablocker  = 1)]
-dataset[grepl("Lithium", Intervention), `:=` (d_lithium  = 1)]
-dataset[grepl("Antihistamine", Intervention), `:=` (d_antihistamine  = 1)]
-dataset[grepl("cycloserine", Intervention), `:=` (d_cycloserine  = 1)]
 
 # Code comparison dummies #
-dataset[grepl("Benzodiazepines", Comparison), `:=` (d_benzodiazepines = d_benzodiazepines-1)]
-dataset[grepl("SSRI", Comparison), `:=` (d_antidepressants = d_antidepressants-1)]
 dataset[grepl("Psychodynamic therapy", Comparison), `:=` (d_psychodynamic = d_psychodynamic-1)]
 dataset[grepl("Behavioural therapy", Comparison), `:=` (d_behavioural_therapy = d_behavioural_therapy-1)]
 dataset[grepl("Prolonged exposure", Comparison), `:=` (d_behavioural_therapy = d_behavioural_therapy-1)]
 dataset[grepl("CBT", Comparison), `:=` (d_cognitive_therapy = d_cognitive_therapy - 1, d_behavioural_therapy = d_behavioural_therapy-1)]
-dataset[grepl("Azapirones", Comparison), `:=` (d_azapirones = d_azapirones - 1)]
 dataset[grepl("Supportive therapy", Comparison), `:=` (d_supportive = d_supportive - 1)]
 
 # Write out datafile #
